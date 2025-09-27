01c6b75a3d9a0b9def713dc9f87f8dba9f6f196c
scallywaag <38130262+scallywaag@users.noreply.github.com>
2025-09-28 00:40:57 +0200

    add notes table migration

--- migrations/20250927194459_add_tables.sql
+++ migrations/20250927194459_add_tables.sql
@@ -1,9 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
+
+CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY,
+    content TEXT NOT NULL,
+    ttl INTEGER NOT NULL DEFAULT 0,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
+DROP TABLE IF EXISTS notes;
 -- +goose StatementEnd
